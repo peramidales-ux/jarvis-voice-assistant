ba37ed8695c901075968fff08d07db7ef827d040
peramidales-ux <peramidales@gmail.com>
2026-07-20 11:03:40 +0600

    feat: cyberpunk color palette

--- app/src/main/java/com/jarvis/assistant/ui/theme/Color.kt
+++ app/src/main/java/com/jarvis/assistant/ui/theme/Color.kt
@@ -2,9 +2,13 @@ package com.jarvis.assistant.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val JarvisBlue = Color(0xFF00D4FF)
-val JarvisBlueDark = Color(0xFF0099CC)
-val JarvisBackground = Color(0xFF0A0A0F)
-val JarvisSurface = Color(0xFF12121A)
-val JarvisText = Color(0xFFE0E0E0)
-val JarvisAccent = Color(0xFF00FFAA)
+val CyberPurple = Color(0xFF9D4EDD)
+val CyberCyan = Color(0xFF00FFFF)
+val CyberPink = Color(0xFFFF006E)
+val CyberGreen = Color(0xFF39FF14)
+val CyberBackgroundDark = Color(0xFF0A0A1A)
+val CyberBackgroundLight = Color(0xFF1A0A2E)
+val CyberSurface = Color(0xFF16162A)
+val CyberTextPrimary = Color(0xFFE0E0FF)
+val CyberTextSecondary = Color(0xFFB0B0CC)
+val CyberTextMuted = Color(0xFF6B6B8A)
